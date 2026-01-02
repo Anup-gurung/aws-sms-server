ff9e1a736c31df3527da346bcbfc7c31c3a8b8b6
Anup-gurung <dragonthunder0127@gmail.com>
2026-01-02 10:35:17 +0600

    server

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 import app from './app.js';
 
+const HOST = '0.0.0.0';
 const PORT = parseInt(process.env.PORT, 10) || 5000;
-app.listen(PORT, () => {
-  console.log(`🚀 SMS API running on port ${PORT}`);
+
+app.listen(PORT, HOST, () => {
+  console.log(`🚀 SMS API running on ${HOST}:${PORT}`);
 });
