af97e41d13647889b6645173301ebeddb20a01b2
Anup-gurung <dragonthunder0127@gmail.com>
2026-01-02 10:18:25 +0600

    server chamges

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import app from './app.js';
 
-const PORT = process.env.PORT || 3000;
+const PORT = parseInt(process.env.PORT, 10) || 5000;
 app.listen(PORT, () => {
   console.log(`🚀 SMS API running on port ${PORT}`);
 });
